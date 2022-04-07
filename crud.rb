1a1c2aaf3fb0abc147241b18f852c549bf7f8653
irendon1993 <isaacrendon7@gmail.com>
2022-04-07 12:27:30 -0400

    Testing authenticate user using bcrypt

--- crud.rb
+++ crud.rb
@@ -12,7 +12,7 @@ def create_hash_digest(password)
   BCrypt::Password.create(password)
 end
 
-def verify_hash_digest(passwords)
+def verify_hash_digest(password)
   BCrypt::Password.new(password)
 end
 
@@ -23,4 +23,17 @@ def create_secure_users(list_of_users)
   list_of_users
 end
 
-puts create_secure_users(users)
+new_users = create_secure_users(users)
+puts new_users
+
+def authenticate_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && 
+      verify_hash_digest(user_record[:password]) == password
+      return user_record
+    end
+  end
+  "Credentials were not correct"
+end 
+
+p authenticate_user("heisenberg","password6",new_users)
