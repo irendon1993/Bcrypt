71732ddaed54b0c3f99dad9349e795b33b444f3b
irendon1993 <isaacrendon7@gmail.com>
2022-04-07 11:47:56 -0400

    Testing password hashing

--- main.rb
+++ main.rb
@@ -21,3 +21,13 @@ puts my_password == "not my password" #=> false
 # my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
 # my_password == "my password"     #=> true
 # my_password == "not my password" #=> false
+
+new_password = BCrypt::Password.create("Test")
+puts new_password
+
+new_password = BCrypt::Password.new("$2a$12$ki58rMxLeNzQv9RlAvX.gO6bG39cwEx9vDwGopHjX5c5.LMvlJeWO")
+if new_password == "Test"
+ puts "Nice"
+else 
+  puts "Damn"
+end
