53d79045c4d66040592dd6e3f22a0a713e0c93f2
irendon1993 <isaacrendon7@gmail.com>
2022-04-07 12:38:16 -0400

    using Crud module from main to hash with bcrypt

--- main.rb
+++ main.rb
@@ -1,35 +1,12 @@
-# Use case for student class - Authentication systems in web applications
-# For now think of students as users
-# Done with usernames and passwords
-# Security for user information very important
-# Passwords are virtually never stored in their string forms
-# Hashed digests are stored in the db's
-# A popular hashing algorithm is MD5 - bcrypt
-# Creates a hash digest of the string
+require_relative 'crud'
 
-require 'bcrypt'
+users = [
+  {username:"mashrur", password:"password1"},
+  {username:"jack", password:"password2"},
+  {username:"arya", password:"password3"},
+  {username:"jonsnow", password:"password4"},
+  {username:"heisenberg", password:"password5"}
+]
 
-my_password = BCrypt::Password.create("my password")
-#=> "$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey"
-
-puts my_password
-puts my_password.version              #=> "2a"
-puts my_password.cost                 #=> 12
-puts my_password == "my password"     #=> true
-puts my_password == "not my password" #=> false
-
-# my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
-# my_password == "my password"     #=> true
-# my_password == "not my password" #=> false
-
-new_password = BCrypt::Password.create("Test")
-puts new_password
-
-new_password = BCrypt::Password.new("$2a$12$ki58rMxLeNzQv9RlAvX.gO6bG39cwEx9vDwGopHjX5c5.LMvlJeWO")
-if new_password == "Test"
- puts "Nice"
-else 
-  puts "Damn"
-end
-
-puts new_password
+hashed_users = Crud.create_secure_users(users)
+puts hashed_users
